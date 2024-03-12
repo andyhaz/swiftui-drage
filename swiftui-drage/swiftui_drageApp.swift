401a7d05168727d2e0d0d1078cc549d994ff1c27
andrew <andyhaz1970@gmail.com>
2024-03-12 17:41:15 -0500

    Initial Commit

---
+++ swiftui-drage/swiftui_drageApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_drageApp.swift
+//  swiftui-drage
+//
+//  Created by andyhaz on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_drageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
